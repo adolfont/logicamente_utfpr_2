3bd348682905856e535f3e15a72870c000c53404
Adolfo Neto <adolfo.usp@gmail.com>
2018-03-15 19:22:06 -0300

    second commit

---
+++ lib/logicamente_utfpr_2.ex
@@ -0,0 +1,18 @@
+defmodule LogicamenteUtfpr2 do
+  @moduledoc """
+  Documentation for LogicamenteUtfpr2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogicamenteUtfpr2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logicamente_utfpr_2_test.exs
@@ -0,0 +1,8 @@
+defmodule LogicamenteUtfpr2Test do
+  use ExUnit.Case
+  doctest LogicamenteUtfpr2
+
+  test "greets the world" do
+    assert LogicamenteUtfpr2.hello() == :world
+  end
+end
